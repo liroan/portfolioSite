b23df65d04f05753d7250d24a1e73b21f20c06ed
Denis <serzhantov-02@mail.ru>
2022-07-21 19:41:19 +0500

    FormPage: I made a page with a form but so far on normal inputsswitching

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Banner from "./views/Banner/Banner";
 import Skills from "./views/Skills/Skills";
 import ProjectsPage from "./views/ProjectsPage/ProjectsPage";
+import FormPage from "./views/FormPage/FormPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Banner />
           <Skills />
           <ProjectsPage />
+          <FormPage />
       </div>
   );
 }
